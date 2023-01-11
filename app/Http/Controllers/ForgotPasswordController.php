546f0c5b8f7f6d100cc84b9ec7a6b50d4200e21d
Michael E Adebisi <adebisienads@gmail.com>
2023-01-11 20:43:29 +0100

    build

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -17,6 +17,8 @@ class ForgotPasswordController extends Controller
     }
 
     public function reset() {
+   
+
         $credentials = request()->validate([
             'email' => 'required|email',
             'token' => 'required|string',
