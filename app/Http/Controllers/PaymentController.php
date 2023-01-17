32e3407f37c512bafb3fa9db4249644ea313ea2b
Michael E Adebisi <adebisienads@gmail.com>
2023-01-17 01:18:16 +0100

    hpay: update payment controller Signed-off-by: Michael E Adebisi <adebisienads@gmail.com>

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -14,26 +14,28 @@ use App\Actions\Proc;
 
 class PaymentController extends Controller
 {
-    
+
 
     // public function __construct(){
     //     $this->middleware('auth');
     // }
-    public function paym(){
+    public function paym()
+    {
         $data['mchid'] = '000801682';
         $data['timestamp'] = time();
         $data['amount'] = '100';
-        $data['orderno'] = intval(microtime(true) * 1000 * 1000);;
+        $data['orderno'] = intval(microtime(true) * 1000 * 1000);
+        ;
         $data['notifyurl '] = 'http://localhost:8000';
         $data['currency'] = 'USD';
-    
-    
-    
-    
+
+
+
+
         $url = 'https://api.hpay.one/open/index/createorder';
         $key = 'HECJKDEtTMbFKQDzVqY9';
-    
-// $curl = new Curl();
+
+        // $curl = new Curl();
 // $curl->setOpt(CURLOPT_RETURNTRANSFER, TRUE);
 // $curl->setOpt(CURLOPT_SSL_VERIFYPEER, FALSE);
 // $curl->to($url)
@@ -41,47 +43,50 @@ class PaymentController extends Controller
 
         $response = Curl::to($url)
 
-                ->withData($data)
+            ->withData($data)
 
-                ->post();
+            ->post();
 
         // $response = $curl;
 
-    dd($response);
+        dd($response);
     }
-    public function pay(Request $request){
-        
+    public function pay(Request $request)
+    {
+
         $process = new Process;
         $user = Auth::user();
         $process->execute($request, $user);
-        
-    }
- 
-    public function testpay(){
-    
 
-    $process2 = new Proc;
-
-    $process2->exec();
-    
     }
 
-    public function callback($result){
+    public function testpay()
+    {
+
+
+        $process2 = new Proc;
+
+        $process2->exec();
+
+    }
+
+    public function callback($result)
+    {
         dd('hit');
-$wallet = Wallet::where('user_id', Auth::id())->first();
-            if($wallet){
+        $wallet = Wallet::where('user_id', Auth::id())->first();
+        if ($wallet) {
             $wallet->update([
                 'order_no' => $result['data']['tx_orderno'],
                 'deposit' => $wallet->deposit + $result['data']['amount'],
             ]);
-        }else{
+        } else {
             Wallet::create([
                 'user_id' => $user->id,
                 'deposit' => $result['data']['amount']
             ]);
         }
-            return $result['data']['pay_info'];
-        }
-    
+        return $result['data']['pay_info'];
+    }
+
 
 }
