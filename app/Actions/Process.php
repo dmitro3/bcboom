938538fc3f459f39c1c39595c98064f78afd2c22
Michael E Adebisi <adebisienads@gmail.com>
2023-01-19 09:42:06 +0100

    ...

--- app/Actions/Process.php
+++ app/Actions/Process.php
@@ -53,7 +53,7 @@ class Process
 
         if (isset($result['data']['pay_info'])) {
             // print('success');
-                // dd($result['data']);
+                dd($result['data']);
             
                $pay = Payment::create([
                     "amount" => $result['data']['amount'],
