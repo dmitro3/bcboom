b9d72140812bd88191dda8b60e42ed0f61161327
Michael E Adebisi <adebisienads@gmail.com>
2023-01-21 21:50:21 +0100

    build

--- app/Http/Controllers/WithdrawalController.php
+++ app/Http/Controllers/WithdrawalController.php
@@ -16,7 +16,10 @@ use Carbon\Carbon;
 class WithdrawalController extends Controller
 {
     //
-
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function handle(Request $request){
         $user = Auth::user();
        
