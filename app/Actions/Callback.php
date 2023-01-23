761bb398538e71622314362a87aa6447216e9f25
Darlington OkorieC <darlingtonokoriec@gmail.com>
2023-01-23 19:20:41 +0100

    changed it

--- app/Actions/Callback.php
+++ app/Actions/Callback.php
@@ -1,89 +1,80 @@
 <?php
+namespace App\Actions;
 
 use App\Models\Payment;
 use App\Models\Wallet;
 use Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\PaymentController;
 
 
-class Callback
-{
-
-    function execute()
-    {
-        $data = $_REQUEST;
-        ;
-
-        unset($data['sign']);
+class Callback{
 
 
-        $key = 'HECJKDEtTMbFKQDzVqY9';
+    function run(){
+//接收参数，
+$data = $_REQUEST;
 
-        $user = Auth::user();
+//接受返回数据验证开始
+//md5验证
+unset($data['sign']);
 
-        $sign = getSignOpen($data, $key);
+$user = Auth::user();
 
+$key = 'HECJKDEtTMbFKQDzVqY9';//商户key
+
+$sign = getSignOpen($data,$key);
+
+if($sign == $data['sign']){
+  // 验签成功
+
+  //PENDING 处理中 SUCCESS完成 FAILURE失败
+  if($data['trade_state'] == 'SUCCESS')
+  {
+
+    $pay = Payment::where('user_id', $user->id)
+        ->where('called', 0)
+        ->where('created_at', 'desc')
+        ->first();
+        $wallet = Wallet::where('user_id', $user->id)->first();
         
-        
-        
-        if ($sign == $data['sign']) {
+            $wallet->update([
+                'order_no' => $pay->tx_orderno,
+                'deposit' => $wallet->deposit + $pay->amount,
+                'total' => $wallet->total + $pay->amount
+            ]);
 
-            $pay = Payment::where('user_id', $user->id)
-            ->where('called', '=', 0)
-            ->where('created_at', 'desc')
-            ->first();
+            $pay->update([
+                'called' => 1,
+                'status' => 'PAY'
+            ]);
+     //改变订单状态，及其他业务修改
+  }
+ 
 
-            if ($pay) {
-                if ($data['trade_state'] == 'SUCCESS') {
-                    $user = Auth::user();
-                    $wallet = Wallet::where('user_id', $user->id)->first();
-                    if ($wallet) {
-                        $wallet->update([
-                            'order_no' => $pay->tx_orderno,
-                            'deposit' => $wallet->deposit + $pay->amount,
-                            'total' => $wallet->total + $pay->amount
-                        ]);
-                    } else {
-                        Wallet::create([
-                            'user_id' => $user->id,
-                            'deposit' => $pay->amount
-                        ]);
-                    }
-                    $pay->update([
-                        'called' => 1,
-                        'status' => 'PAY'
-                    ]);
+  return "SUCCESS";
+  //接收通知后必须输出”SUCCESS“代表接收成功。
+}
 
-                    return response()->json([
-                        'user' => $user,
-                        'message' => 'Payment successful',
-                    ], 200);
-                }
-
-
-            }
-            echo "SUCCESS";
-
-        }
-
-        /**
-         *  生成签名
-         */
-        function getSignOpen($Obj, $key)
+    }
+/**
+        *  生成签名
+        */
+        function getSignOpen($Obj,$key) 
         {
             foreach ($Obj as $k => $v) {
-                if (isset($v) && strlen($v) > 0) {
+                if(isset($v) && strlen($v) > 0){
                     $Parameters[$k] = $v;
                 }
             }
             //签名步骤一：按字典序排序参数
             ksort($Parameters);
             $String = formatQueryParaMapOpen($Parameters);
-            $String = $String . '&key=' . $key;
+            $String = $String.'&key='.$key;
             //dlog($String);
             //签名步骤三：MD5加密
             $String = md5($String);
@@ -91,10 +82,12 @@ class Callback
             $result = strtoupper($String);
             return $result;
         }
+
+
         /**
          *  作用：格式化参数，签名过程需要使用
          */
-        function formatQueryParaMapOpen($paraMap, $urlencode = 'false')
+        function formatQueryParaMapOpen($paraMap, $urlencode = 'false') 
         {
             $buff = "";
             ksort($paraMap);
@@ -102,16 +95,15 @@ class Callback
                 // if($urlencode) {
                 //     $v = urlencode($v);
                 // }
-                if ($k != 's') {
-                    $buff .= $k . '=' . $v . '&';
+                if ($k != 's'){
+                    $buff .= $k .'='. $v.'&' ;
                 }
             }
             $reqPar;
             if (strlen($buff) > 0) {
-                $reqPar = substr($buff, 0, strlen($buff) - 1);
+                $reqPar = substr($buff, 0, strlen($buff)-1);
             }
             return $reqPar;
         }
+
     }
-}
-?>
