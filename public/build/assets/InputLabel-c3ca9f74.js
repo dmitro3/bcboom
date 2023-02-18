979662986c2062282084c19913d5151e6bf05b47
Michael E Adebisi <adebisienads@gmail.com>
2023-02-18 21:01:10 +0100

    build

--- public/build/assets/InputLabel-c3ca9f74.js
+++
@@ -1,1 +0,0 @@
-import{a as m}from"./app-e972feae.js";function l({forInput:e,value:t,className:r,children:a}){return m("label",{htmlFor:e,className:"block font-medium text-sm text-gray-700 "+r,children:t||a})}export{l as I};
