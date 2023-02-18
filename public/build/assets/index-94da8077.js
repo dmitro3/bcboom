c2f37e2058637561595f8deb6e8b461af003fb8f
Michael E Adebisi <adebisienads@gmail.com>
2023-02-18 21:54:55 +0100

    build

---
+++ public/build/assets/index-94da8077.js
@@ -0,0 +1,1 @@
+import{a as t,B as m}from"./app-0a34ad46.js";const s=({customStyles:e,children:r})=>t(m,{sx:{width:{xs:"90%",md:"48%"},height:{xs:"11rem",md:"12.75rem"},borderRadius:"1.25rem",display:"flex",alignItems:"center",justifyContent:"center",textAlign:"center",flexDirection:"column",padding:"2rem",mb:{xs:"1rem",md:"0"},...e},children:r});export{s as P};
