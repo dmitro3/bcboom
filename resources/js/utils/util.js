0a7290153ffdd3387f89e8e03211df7282e9494f
Michael E Adebisi <adebisienads@gmail.com>
2023-02-18 21:54:04 +0100

    ewrt
    Signed-off-by: Michael E Adebisi <adebisienads@gmail.com>

--- resources/js/utils/util.js
+++ resources/js/utils/util.js
@@ -196,7 +196,7 @@ export const toggleRollUnder = (gameData, dispatch, setGameData) => {
     );
 
     const multiplier =
-        chance.type === "under" ? 1 : gameData.winChance > 60 ? 0.1 : 20;
+        chance.type === "under" ? 1 : gameData.winChance > 60 ? 10 : 20;
     let payout = Math.abs(
         (100 / (gameData.winChance * multiplier)) *
             (gameData.betAmount - 50 / 100)
@@ -221,8 +221,21 @@ const payoutFunc = (gameData, rollUnder, chance) => {
         differenceInChance[1] - differenceInChance[0]
     );
 
-    const multiplier =
-        rollUnder.type === "under" ? 1 : gameData.winChance > 60 ? 0.1 : 20;
+    let multiplier = 1;
+    switch (rollUnder.type) {
+        case "under":
+            multiplier = 1;
+            break;
+        case "over":
+            multiplier = 225;
+            if (gameData.winChance < 10) {
+                multiplier = 900;
+            } else if (gameData.winChance > 50) {
+                multiplier = 0.25;
+            } else if (gameData.winChance > 60) {
+                multiplier = 0.15;
+            } else if (gameData.winChance > 80) multiplier = 0.1;
+    }
     let payout = Math.abs(
         (100 / (gameData.winChance * multiplier)) *
             (gameData.betAmount - 50 / 100)
@@ -259,6 +272,3 @@ export const calcPayout = (gameData, dispatch, setGameData) => {
         })
     );
 };
-
-
-
